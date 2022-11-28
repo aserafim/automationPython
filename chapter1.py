b580a9981171e6b23da3bb582c9841338228a043
aserafim <correia.alefe@gmail.com>
2022-11-28 18:25:26 -0300

    Exercícios do Capítulo 1

---
+++ chapter1.py
@@ -0,0 +1,23 @@
+#Testando inserção de input do usuário
+#print("Entre com o endereço IP: ");
+#enderecoIP = input();
+
+#print("O endereço digitado foi: ", enderecoIP);
+'''
+spam = -8;
+
+if(spam == 1):
+    print("Hello");
+elif (spam ==2):
+    print("Howdy")
+else:
+    print("Greetings!")
+'''
+
+#for i in range(0,10):
+#    print(i+1);
+
+i=0;
+while (i<10):
+    print(i+1);
+    i = i+1;
