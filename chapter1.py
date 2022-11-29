2e8fc53e3377afd1d052bec29fa48acaf386c835
aserafim <correia.alefe@gmail.com>
2022-11-29 17:19:18 -0300

    Capítulo 1 resolvido

--- chapter1.py
+++ chapter1.py
@@ -33,33 +33,54 @@ valor na tela. Uma expressão é um conjunto de operadores, variáveis e
 chamadas de funções(instruções) que resultam em um valor.
 
 
-6 - 
+6 - O que a variável bacon conerá após o código a seguir ser executado?
+
+bacon = 20
+bacon + 1
+
+bacon conterá o valor 21
+
+
+
+7 - Para que valores as duas expressões a seguir serão avaliadas?
+
+'spam' + 'spamspam'
+'spam' * 3
+
+Nos dois casos o resultado será 'spamspamspam'
+
+
+
+8 - Por que eggs é um nome válido de variável enqunto 100 é inválido?
+
+O Python não pemite que variáveis sejam noemadas iniciando com um caractere
+numérico.
+
+
+
+9 - Quais três funções podem ser usadas para obter uma versão inteira de ponto flutuante
+ou em string de um valor?
+
+Supondo que numero seja a variável, temos:
+
+int(numero)
+float(numero)
+str(numero)
+
+
+
+10 - Por que a expressão a seguir causa um erro? Como podemos corrigi-la?
+'I have eaten' + 99 + 'burritos.'
+
+Ao executar a instrução acima o Python vai tentar realizar uma operação de 
+adição com as expressões 'I have eaten' que é uma string e a expressão 99, 
+que é um inteiro. Isso não é possível, portanto o Python retornará um erro.
+Para corrigir o problema podemos reescrever a instrução como a seguir:
+
+'I have eaten' + '99' + 'burritos.'
+
+Ou ainda,
+
+'I have eaten' + str(99) + 'burritos.'
 
 '''
-
-
-
-
-
-
-
-
-#print("O endereço digitado foi: ", enderecoIP);
-'''
-spam = -8;
-
-if(spam == 1):
-    print("Hello");
-elif (spam ==2):
-    print("Howdy")
-else:
-    print("Greetings!")
-'''
-
-#for i in range(0,10):
-#    print(i+1);
-
-#i=0;
-#while (i<10):
-#    print(i+1);
-#    i = i+1;
