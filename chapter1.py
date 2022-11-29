26e04dfe06536b30334e1aaeb961f10b08803df3
aserafim <correia.alefe@gmail.com>
2022-11-29 17:06:02 -0300

    Resolvendo exercícios do capítulo 1

--- chapter1.py
+++ chapter1.py
@@ -1,6 +1,48 @@
-#Testando inserção de input do usuário
-#print("Entre com o endereço IP: ");
-#enderecoIP = input();
+# Capítulo 1 - Básico sobre Python
+# Autor: aserafim
+
+'''
+1 - Quais das opções abaixo são operadores e quais são valores?
+# operadores: * - / +
+# valores : 'hello' -88.8 5
+
+
+2 - Qual das opções a seguir é uma variável e qual é uma string?
+
+spam - variável de nome spam
+'spam' - valor que pode ser atribuído a qualquer variável do tipo string
+
+
+3 - Nomeie três tipos de dados.
+
+1 - int
+2.0 - float
+'texto' - string
+
+
+4 - De que é composta uma expressão? O que fazem as expressões?
+
+Uma expressão é composta de operadores, variáveis e chamadas de funções.
+As expressões são utilizadas para dar instruições ao Python.
+
+
+5 - Qual a diferença entre uma expressão e uma instrução?
+
+Uma instrução é um comando, por exemplo print, que exibe um
+valor na tela. Uma expressão é um conjunto de operadores, variáveis e 
+chamadas de funções(instruções) que resultam em um valor.
+
+
+6 - 
+
+'''
+
+
+
+
+
+
+
 
 #print("O endereço digitado foi: ", enderecoIP);
 '''
@@ -17,7 +59,7 @@ else:
 #for i in range(0,10):
 #    print(i+1);
 
-i=0;
-while (i<10):
-    print(i+1);
-    i = i+1;
+#i=0;
+#while (i<10):
+#    print(i+1);
+#    i = i+1;
